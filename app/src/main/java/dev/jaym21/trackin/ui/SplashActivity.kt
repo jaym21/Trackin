4c4f0b4e63a6f61b4b63b1596037f5551511cf01
jaym21 <jaynmistry21@gmail.com>
2022-05-24 09:49:22 +0530

    added splash screen activity with checking location permissions

---
+++ app/src/main/java/dev/jaym21/trackin/ui/SplashActivity.kt
@@ -0,0 +1,96 @@
+package dev.jaym21.trackin.ui
+
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import dev.jaym21.trackin.util.Constants
+
+class SplashActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val isFineLocPermissionGranted = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+        val isCoarseLocPermissionGranted = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED
+        val isBackgroundLocPermissionGranted = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_BACKGROUND_LOCATION) == PackageManager.PERMISSION_GRANTED
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            if (isFineLocPermissionGranted) {
+                if (isCoarseLocPermissionGranted) {
+                    if (isBackgroundLocPermissionGranted) {
+                        val intent = Intent(this, MainActivity::class.java)
+                        startActivity(intent)
+                        finish()
+                    } else {
+                        ActivityCompat.requestPermissions(this,  arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), Constants.ACCESS_FINE_LOCATION_REQUEST_CODE)
+                    }
+                } else {
+                    ActivityCompat.requestPermissions(this,  arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION), Constants.ACCESS_COARSE_LOCATION_REQUEST_CODE)
+                }
+            } else {
+                ActivityCompat.requestPermissions(this,  arrayOf(Manifest.permission.ACCESS_BACKGROUND_LOCATION), Constants.ACCESS_BACKGROUND_LOCATION_REQUEST_CODE)
+            }
+
+        } else {
+            if (isFineLocPermissionGranted) {
+                if (isCoarseLocPermissionGranted) {
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                } else {
+                    ActivityCompat.requestPermissions(this,  arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), Constants.ACCESS_FINE_LOCATION_REQUEST_CODE)
+                }
+            } else {
+                ActivityCompat.requestPermissions(this,  arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION), Constants.ACCESS_COARSE_LOCATION_REQUEST_CODE)
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when(requestCode) {
+            Constants.ACCESS_FINE_LOCATION_REQUEST_CODE -> {
+                if (grantResults.isEmpty() || grantResults[0] == PackageManager.PERMISSION_DENIED) {
+//                    val intent = Intent(this, NoPermissionActivity::class.java)
+//                    startActivity(intent)
+//                    finish()
+                }else {
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                }
+            }
+            Constants.ACCESS_COARSE_LOCATION_REQUEST_CODE -> {
+                if (grantResults.isEmpty() || grantResults[0] == PackageManager.PERMISSION_DENIED) {
+//                    val intent = Intent(this, NoPermissionActivity::class.java)
+//                    startActivity(intent)
+//                    finish()
+                }else {
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                }
+            }
+            Constants.ACCESS_BACKGROUND_LOCATION_REQUEST_CODE -> {
+                if (grantResults.isEmpty() || grantResults[0] == PackageManager.PERMISSION_DENIED) {
+//                    val intent = Intent(this, NoPermissionActivity::class.java)
+//                    startActivity(intent)
+//                    finish()
+                }else {
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                }
+            }
+        }
+    }
+}
--- app/src/main/java/dev/jaym21/trackin/util/Constants.kt
+++ app/src/main/java/dev/jaym21/trackin/util/Constants.kt
@@ -3,4 +3,7 @@ package dev.jaym21.trackin.util
 object Constants {
 
     const val DATABASE_NAME = "trackin_database"
+    const val ACCESS_FINE_LOCATION_REQUEST_CODE = 100
+    const val ACCESS_COARSE_LOCATION_REQUEST_CODE = 101
+    const val ACCESS_BACKGROUND_LOCATION_REQUEST_CODE = 102
 }
