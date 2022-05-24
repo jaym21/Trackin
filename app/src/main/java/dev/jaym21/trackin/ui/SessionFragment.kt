2c954195b1a9fe007949a136351b406eb9518ee6
jaym21 <jaynmistry21@gmail.com>
2022-05-24 10:03:51 +0530

    added mapView to session fragment

--- app/src/main/java/dev/jaym21/trackin/ui/SessionFragment.kt
+++ app/src/main/java/dev/jaym21/trackin/ui/SessionFragment.kt
@@ -6,14 +6,29 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import dev.jaym21.trackin.R
+import dev.jaym21.trackin.databinding.FragmentSessionBinding
 
 class SessionFragment : Fragment() {
+
+    private var _binding: FragmentSessionBinding? = null
+    private val binding: FragmentSessionBinding
+        get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_session, container, false)
+        _binding = FragmentSessionBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
