d53562eb2d36268484e23c54ba332c968373a237
jaym21 <jaynmistry21@gmail.com>
2022-05-28 18:57:57 +0530

    implemented notification click

--- app/src/main/java/dev/jaym21/trackin/ui/MainActivity.kt
+++ app/src/main/java/dev/jaym21/trackin/ui/MainActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
 import dagger.hilt.android.AndroidEntryPoint
 import dev.jaym21.trackin.R
 import dev.jaym21.trackin.databinding.ActivityMainBinding
@@ -15,11 +17,16 @@ class MainActivity : AppCompatActivity() {
     private var _binding: ActivityMainBinding? = null
     private val binding: ActivityMainBinding
         get() = _binding!!
+    private var navHostFragment: NavHostFragment? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+
 
         navigateToSessionFragmentIfRequired(intent)
     }
@@ -28,10 +35,13 @@ class MainActivity : AppCompatActivity() {
         super.onNewIntent(intent)
         navigateToSessionFragmentIfRequired(intent)
     }
-
+    //TODO: fix crash when notification is clicked after app is destroyed
     private fun navigateToSessionFragmentIfRequired(intent: Intent?) {
-        if (intent?.action == Constants.ACTION_SHOW_SESSION_FRAGMENT) {
-            binding.navHostFragment.findNavController().navigate(R.id.action_global_sessionFragment)
+        if (navHostFragment != null) {
+            if (intent?.action == Constants.ACTION_SHOW_SESSION_FRAGMENT) {
+                navHostFragment!!.findNavController()
+                    .navigate(R.id.action_global_sessionFragment)
+            }
         }
     }
 
