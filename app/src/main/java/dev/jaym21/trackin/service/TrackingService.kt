93ea074cf7755fcf2f3f0107d994eb1ba689e38f
jaym21 <jaynmistry21@gmail.com>
2022-05-29 11:14:32 +0530

    implemented function to request user's current location

--- app/src/main/java/dev/jaym21/trackin/service/TrackingService.kt
+++ app/src/main/java/dev/jaym21/trackin/service/TrackingService.kt
@@ -1,6 +1,7 @@
 package dev.jaym21.trackin.service
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
@@ -8,10 +9,12 @@ import android.content.Context
 import android.content.Intent
 import android.location.Location
 import android.os.Build
+import android.os.Looper
 import android.util.Log
 import androidx.core.app.NotificationCompat
 import androidx.lifecycle.LifecycleService
 import androidx.lifecycle.MutableLiveData
+import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationCallback
 import com.google.android.gms.location.LocationRequest
 import com.google.android.gms.location.LocationResult
@@ -36,10 +39,17 @@ class TrackingService: LifecycleService() {
         Manifest.permission.ACCESS_BACKGROUND_LOCATION,
     )
     var isFirstRun = true
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+
 
     companion object {
         val isTracking: MutableLiveData<Boolean> = MutableLiveData()
         val pathPoints: MutableLiveData<Polylines> = MutableLiveData()
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        fusedLocationProviderClient = FusedLocationProviderClient(this)
     }
 
     private fun initValues() {
@@ -98,7 +108,25 @@ class TrackingService: LifecycleService() {
         startForeground(Constants.NOTIFICATION_ID, notificationBuilder.build())
     }
 
-    val locationCallback = object : LocationCallback() {
+    @SuppressLint("MissingPermission")
+    private fun updateLocationTracking(isTracking: Boolean) {
+        if (isTracking) {
+            if (checkIfPermissionsGranted()) {
+                //if currently tracking and location permissions are available, then requesting user's location update
+                val locationRequest = LocationRequest.create().apply {
+                    interval = Constants.LOCATION_UPDATE_INTERVAL
+                    fastestInterval = Constants.FASTEST_LOCATION_INTERVAL
+                    priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+                }
+                fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, Looper.getMainLooper())
+            }
+        } else {
+            //removing request for location update
+            fusedLocationProviderClient.removeLocationUpdates(locationCallback)
+        }
+    }
+
+    private val locationCallback = object : LocationCallback() {
         override fun onLocationResult(result: LocationResult) {
             super.onLocationResult(result)
             if (isTracking.value!!) {
@@ -109,25 +137,6 @@ class TrackingService: LifecycleService() {
                 }
             }
         }
-    }
-
-    private fun updateLocationTracking(isTracking: Boolean) {
-        if (isTracking) {
-            if (checkIfPermissionsGranted()) {
-                val locationRequest = LocationRequest.create().apply {
-                    interval = Constants.LOCATION_UPDATE_INTERVAL
-                    fastestInterval = Constants.FASTEST_LOCATION_INTERVAL
-                    priority = LocationRequest.PRIORITY_HIGH_ACCURACY
-                }
-            }
-        }
-    }
-
-    private fun createNotificationChannel(notificationManager: NotificationManager) {
-        if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.O)
-            return
-        val channel = NotificationChannel(Constants.NOTIFICATION_CHANNEL_ID, Constants.NOTIFICATION_CHANNEL_NAME, NotificationManager.IMPORTANCE_LOW)
-        notificationManager.createNotificationChannel(channel)
     }
 
     private fun addEmptyPolyline() {
@@ -145,6 +154,13 @@ class TrackingService: LifecycleService() {
                 pathPoints.postValue(this)
             }
         }
+    }
+
+    private fun createNotificationChannel(notificationManager: NotificationManager) {
+        if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.O)
+            return
+        val channel = NotificationChannel(Constants.NOTIFICATION_CHANNEL_ID, Constants.NOTIFICATION_CHANNEL_NAME, NotificationManager.IMPORTANCE_LOW)
+        notificationManager.createNotificationChannel(channel)
     }
 
     private fun checkIfPermissionsGranted(): Boolean =
