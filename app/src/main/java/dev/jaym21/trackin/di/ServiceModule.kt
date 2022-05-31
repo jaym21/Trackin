c055a248a7f7f3fa8537328381e52aceeb7b1dd8
jaym21 <jaynmistry21@gmail.com>
2022-05-31 10:37:50 +0530

    adding a dagger module for dependency injection in tracking service

---
+++ app/src/main/java/dev/jaym21/trackin/di/ServiceModule.kt
@@ -0,0 +1,12 @@
+package dev.jaym21.trackin.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ServiceComponent
+
+@Module
+@InstallIn(ServiceComponent::class)
+object ServiceModule {
+
+
+}
