a717bb0a6627e44c7e8fe729a711f5c7e9b6ea10
jaym21 <jaynmistry21@gmail.com>
2022-06-01 10:28:20 +0530

    implemented session recycler adapter

--- app/src/main/java/dev/jaym21/trackin/adapter/SessionRVAdapter.kt
+++ app/src/main/java/dev/jaym21/trackin/adapter/SessionRVAdapter.kt
@@ -1,11 +1,18 @@
 package dev.jaym21.trackin.adapter
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import dev.jaym21.trackin.R
 import dev.jaym21.trackin.model.Session
+import dev.jaym21.trackin.util.Utilities
 
 class SessionRVAdapter(private val listener: ISessionRVAdapter): ListAdapter<Session, SessionRVAdapter.SessionViewHolder>(SessionDiffUtil()) {
 
@@ -20,15 +27,30 @@ class SessionRVAdapter(private val listener: ISessionRVAdapter): ListAdapter<Ses
     }
 
     inner class SessionViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
-
+        val root: ConstraintLayout = itemView.findViewById(R.id.clSessionRoot)
+        val image: ImageView = itemView.findViewById(R.id.ivSessionImage)
+        val date: TextView = itemView.findViewById(R.id.tvSessionDate)
+        val distance: TextView = itemView.findViewById(R.id.tvSessionDistance)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SessionViewHolder {
-
+        return SessionViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.session_item_layout, parent, false))
     }
 
     override fun onBindViewHolder(holder: SessionViewHolder, position: Int) {
+        val currentItem = getItem(position)
+        holder.apply {
+            Glide.with(itemView.context).load(currentItem.sessionImage).into(image)
 
+            val formattedDate = Utilities.convertDateFormat(currentItem.runTimeInMillis)
+            date.text = formattedDate
+
+            distance.text = "${currentItem.distanceInMeters / 1000L}"
+
+            root.setOnClickListener {
+                listener.onSessionClick(currentItem)
+            }
+        }
     }
 }
 
