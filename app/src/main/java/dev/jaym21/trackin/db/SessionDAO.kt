cb6acd6d4d7e17e3e2cf1e240574b8d687ef16fc
jaym21 <jaynmistry21@gmail.com>
2022-06-03 11:48:05 +0530

    added functions in DAO to get total distance, calories, average speed and session time

--- app/src/main/java/dev/jaym21/trackin/db/SessionDAO.kt
+++ app/src/main/java/dev/jaym21/trackin/db/SessionDAO.kt
@@ -15,4 +15,16 @@ interface SessionDAO {
 
     @Query("SELECT * FROM session_table ORDER BY timestamp DESC")
     fun getAllSessionsOrderByDate(): LiveData<List<Session>>
+
+    @Query("SELECT SUM(distanceInMeters) FROM session_table")
+    fun getTotalDistance(): LiveData<Int>
+
+    @Query("SELECT SUM(caloriesBurned) FROM session_table")
+    fun getTotalCalories(): LiveData<Int>
+
+    @Query("SELECT SUM(avgSpeedInKMH) FROM session_table")
+    fun getTotalAverageSpeed(): LiveData<Float>
+
+    @Query("SELECT SUM(sessionTimeInMillis) FROM session_table")
+    fun getTotalSessionTime(): LiveData<Int>
 }
--- app/src/main/java/dev/jaym21/trackin/model/Session.kt
+++ app/src/main/java/dev/jaym21/trackin/model/Session.kt
@@ -10,7 +10,7 @@ data class Session (
     var distanceInMeters: Int = 0,
     var avgSpeedInKMH: Float = 0F,
     var caloriesBurned: Int = 0,
-    var runTimeInMillis: Long = 0L,
+    var sessionTimeInMillis: Long = 0L,
     var timestamp: Long = 0L
 ) {
         @PrimaryKey(autoGenerate = true)
