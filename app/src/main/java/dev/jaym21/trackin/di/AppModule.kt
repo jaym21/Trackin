0e90e2ff61eebcd46160abcaad4e06df54f6e706
jaym21 <jaynmistry21@gmail.com>
2022-06-03 10:31:40 +0530

    added providers for saved sharedPreference variables

--- app/src/main/java/dev/jaym21/trackin/di/AppModule.kt
+++ app/src/main/java/dev/jaym21/trackin/di/AppModule.kt
@@ -3,6 +3,7 @@ package dev.jaym21.trackin.di
 import android.app.Application
 import android.content.Context
 import android.content.Context.MODE_PRIVATE
+import android.content.SharedPreferences
 import androidx.room.Room
 import dagger.Module
 import dagger.Provides
@@ -33,4 +34,19 @@ object AppModule {
     @Singleton
     fun provideSharedPreferences(@ApplicationContext application: Context) =
         application.getSharedPreferences(Constants.SHARED_PREFERENCES_TRACKIN, MODE_PRIVATE)
+
+    @Provides
+    @Singleton
+    fun provideUserName(sharedPreferences: SharedPreferences) =
+        sharedPreferences.getString(Constants.USER_NAME, "") ?: ""
+
+    @Provides
+    @Singleton
+    fun provideUserWeight(sharedPreferences: SharedPreferences) =
+        sharedPreferences.getFloat(Constants.USER_WEIGHT, 70f)
+
+    @Provides
+    @Singleton
+    fun provideIsFirstRun(sharedPreferences: SharedPreferences) =
+        sharedPreferences.getBoolean(Constants.IS_FIRST_RUN, true)
 }
