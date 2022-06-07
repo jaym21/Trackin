510246adaad78aeaf36933aff39d14ef0db02e81
jaym21 <jaynmistry21@gmail.com>
2022-06-07 11:42:46 +0530

    made session data class parcelable

--- app/src/main/java/dev/jaym21/trackin/model/Session.kt
+++ app/src/main/java/dev/jaym21/trackin/model/Session.kt
@@ -1,9 +1,14 @@
 package dev.jaym21.trackin.model
 
 import android.graphics.Bitmap
+import android.os.Parcelable
+import androidx.annotation.Keep
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import kotlinx.parcelize.Parcelize
 
+@Keep
+@Parcelize
 @Entity(tableName = "session_table")
 data class Session (
     var sessionImage: Bitmap? = null,
@@ -12,7 +17,7 @@ data class Session (
     var caloriesBurned: Int = 0,
     var sessionTimeInMillis: Long = 0L,
     var timestamp: Long = 0L
-) {
+) : Parcelable {
         @PrimaryKey(autoGenerate = true)
         var id: Int? = null
 }
