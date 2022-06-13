5722b251216f6e7e22ecac226955116009cf4cad
jaym21 <jaynmistry21@gmail.com>
2022-06-13 10:52:19 +0530

    implementing charts for overall statistics

--- app/src/main/java/dev/jaym21/trackin/ui/StatisticsFragment.kt
+++ app/src/main/java/dev/jaym21/trackin/ui/StatisticsFragment.kt
@@ -16,6 +16,9 @@ import dagger.hilt.android.AndroidEntryPoint
 import dev.jaym21.trackin.R
 import dev.jaym21.trackin.databinding.FragmentStatisticsBinding
 import dev.jaym21.trackin.model.Session
+import dev.jaym21.trackin.util.CaloriesMarkerView
+import dev.jaym21.trackin.util.DistanceMarkerView
+import dev.jaym21.trackin.util.TimeMarkerView
 import dev.jaym21.trackin.util.Utilities
 import dev.jaym21.trackin.viewmodel.StatisticsViewModel
 
@@ -26,7 +29,9 @@ class StatisticsFragment : Fragment() {
     private val binding: FragmentStatisticsBinding
         get() = _binding!!
     private val statisticsViewModel: StatisticsViewModel by viewModels()
-    private val xAxisLabelTime = ArrayList<String>()
+    private lateinit var distanceMarkerView: DistanceMarkerView
+    private lateinit var caloriesMarkerView: CaloriesMarkerView
+    private lateinit var timeMarkerView: TimeMarkerView
     private val xAxisLabelSpeed = ArrayList<String>()
 
     override fun onCreateView(
@@ -41,6 +46,10 @@ class StatisticsFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        distanceMarkerView = DistanceMarkerView(requireContext(), R.layout.distance_marker_view)
+        caloriesMarkerView = CaloriesMarkerView(requireContext(), R.layout.calories_marker_view)
+        timeMarkerView = TimeMarkerView(requireContext(), R.layout.time_marker_view)
+
         statisticsViewModel.sessionsOrderByDate.observe(viewLifecycleOwner) {
 
             if (it.isNullOrEmpty()) {
@@ -50,7 +59,7 @@ class StatisticsFragment : Fragment() {
                 binding.tvCaloriesText.visibility = View.GONE
                 binding.caloriesLineChart.visibility = View.GONE
                 binding.tvTimeText.visibility = View.GONE
-                binding.timeBarChart.visibility = View.GONE
+                binding.timeLineChart.visibility = View.GONE
                 binding.tvSpeedText.visibility = View.GONE
                 binding.speedBarChart.visibility = View.GONE
             } else {
@@ -60,16 +69,31 @@ class StatisticsFragment : Fragment() {
                 binding.tvCaloriesText.visibility = View.VISIBLE
                 binding.caloriesLineChart.visibility = View.VISIBLE
                 binding.tvTimeText.visibility = View.VISIBLE
-                binding.timeBarChart.visibility = View.VISIBLE
+                binding.timeLineChart.visibility = View.VISIBLE
                 binding.tvSpeedText.visibility = View.VISIBLE
                 binding.speedBarChart.visibility = View.VISIBLE
 
-                val sessions = if (it.size > 6) {
-                    it.subList(0, 6)
-                } else {
-                    it
-                }
-                val revSessions = sessions.reversed()
+//                val sessions = if (it.size > 6) {
+//                    it.subList(0, 6)
+//                } else {
+//                    it
+//                }
+//                val revSessions = sessions.reversed()
+//
+//                setUpDistanceLineChart(revSessions)
+//                setUpCaloriesLineChart(revSessions)
+//                setUpTimeBarChart(revSessions)
+//                setUpSpeedBarChart(revSessions)
+
+                val revSessions = mutableListOf<Session>()
+
+                revSessions.add(Session(null, 2500, 24.1f, 200, 65158, 1654664281000))
+                revSessions.add(Session(null, 4000, 35f, 350, 351651, 1654664281000))
+                revSessions.add(Session(null, 1400, 15.6f, 150, 13565, 1654750681000))
+                revSessions.add(Session(null, 2800, 28.5f, 250, 165165, 1654837081000))
+                revSessions.add(Session(null, 1600, 18.9f, 150, 645165, 1654923481000))
+                revSessions.add(Session(null, 3480, 33f, 300, 48645, 1655009881000))
+                revSessions.add(Session(null, 2100, 20f, 180, 65158, 1655096281000))
 
                 setUpDistanceLineChart(revSessions)
                 setUpCaloriesLineChart(revSessions)
@@ -99,6 +123,7 @@ class StatisticsFragment : Fragment() {
         binding.distanceLineChart.isDragEnabled = true
         binding.distanceLineChart.setScaleEnabled(false)
         binding.distanceLineChart.setPinchZoom(false)
+        binding.distanceLineChart.marker = distanceMarkerView
 
         val lineDataSet = LineDataSet(entriesDistance, "Distance")
 
@@ -138,6 +163,7 @@ class StatisticsFragment : Fragment() {
         binding.caloriesLineChart.isDragEnabled = true
         binding.caloriesLineChart.setScaleEnabled(false)
         binding.caloriesLineChart.setPinchZoom(false)
+        binding.caloriesLineChart.marker = caloriesMarkerView
 
         val lineDataSet = LineDataSet(entriesCalories, "Calories")
 
@@ -162,55 +188,43 @@ class StatisticsFragment : Fragment() {
     }
 
     private fun setUpTimeBarChart(sessions: List<Session>) {
-        binding.timeBarChart.setDrawGridBackground(false)
-        binding.timeBarChart.setDrawBarShadow(false)
-        binding.timeBarChart.setDrawBorders(false)
-        binding.timeBarChart.animateY(1000)
-        binding.timeBarChart.setDrawValueAboveBar(true)
-        binding.timeBarChart.description.isEnabled = false
-        binding.timeBarChart.legend.isEnabled = false
-        binding.timeBarChart.setExtraOffsets(0f, 0f, 0f, 10f)
-        binding.timeBarChart.xAxis.textColor = ContextCompat.getColor(requireContext(), R.color.white)
-        binding.timeBarChart.axisLeft.textColor = ContextCompat.getColor(requireContext(), R.color.white)
+        val entriesCalories = arrayListOf<Entry>()
 
         sessions.forEach {
-            xAxisLabelTime.add("${Utilities.getDateFromTimestamp(it.timestamp)}'${Utilities.getMonthFullName(it.timestamp).substring(0,3)}")
+            entriesCalories.add(Entry(it.timestamp.toFloat(), it.sessionTimeInMillis.toFloat()))
         }
 
-        val xAxis = binding.timeBarChart.xAxis
-        xAxis.isGranularityEnabled = true
-        xAxis.granularity = 1f
-        xAxis.setDrawGridLines(false)
-        xAxis.setDrawAxisLine(false)
-        xAxis.setCenterAxisLabels(false)
-        xAxis.position = XAxis.XAxisPosition.BOTTOM
-        xAxis.textSize = 12f
-        xAxis.labelCount = xAxisLabelTime.size
-        xAxis.valueFormatter = IndexAxisValueFormatter(xAxisLabelTime)
+        binding.timeLineChart.axisLeft?.isEnabled = false
+        binding.timeLineChart.axisRight?.isEnabled = false
+        binding.timeLineChart.xAxis?.isEnabled = false
+        binding.timeLineChart.legend?.isEnabled = false
+        binding.timeLineChart.description?.isEnabled = false
+        binding.timeLineChart.setTouchEnabled(true)
+        binding.timeLineChart.isDragEnabled = true
+        binding.timeLineChart.setScaleEnabled(false)
+        binding.timeLineChart.setPinchZoom(false)
+        binding.timeLineChart.marker = timeMarkerView
 
-        val leftAxis = binding.timeBarChart.axisLeft
-        leftAxis.setDrawAxisLine(true)
-        leftAxis.setDrawGridLines(false)
-        leftAxis.setDrawLabels(true)
-        val rightAxis = binding.timeBarChart.axisRight
-        rightAxis.setDrawAxisLine(false)
-        rightAxis.setDrawGridLines(false)
-        rightAxis.setDrawLabels(false)
+        val lineDataSet = LineDataSet(entriesCalories, "Time")
 
-        val entries = ArrayList<BarEntry>()
-        var i = 0
-        sessions.forEach {
-            entries.add(BarEntry(i.toFloat(), it.sessionTimeInMillis.toFloat()))
-            i++
-        }
+        lineDataSet.lineWidth = 2f
+        lineDataSet.setDrawFilled(true)
+        lineDataSet.setDrawHighlightIndicators(false)
+        lineDataSet.setDrawCircleHole(false)
+        lineDataSet.setDrawCircles(false)
+        lineDataSet.setDrawValues(false)
+        lineDataSet.setDrawIcons(false)
+        lineDataSet.disableDashedLine()
+        lineDataSet.color = ContextCompat.getColor(binding.root.context, R.color.green)
+        lineDataSet.fillDrawable = ContextCompat.getDrawable(binding.root.context, R.drawable.chart_fade_green)
 
-        val barDataSet = BarDataSet(entries, "")
-        barDataSet.color = ContextCompat.getColor(requireContext(), R.color.green)
-        barDataSet.setDrawValues(false)
-        val barData = BarData(barDataSet)
-        barData.barWidth = 0.5f
-        binding.timeBarChart.data = barData
-        binding.timeBarChart.invalidate()
+        val data = LineData(lineDataSet)
+
+        data.setDrawValues(false)
+
+        binding.timeLineChart.data = data
+        binding.timeLineChart.animateXY(2000, 2000, Easing.EaseInCubic)
+        binding.timeLineChart.invalidate()
     }
 
     private fun setUpSpeedBarChart(sessions: List<Session>) {
