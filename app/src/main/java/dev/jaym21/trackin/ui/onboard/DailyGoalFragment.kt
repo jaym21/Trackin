ef48fa0208489160f30440e3f34918d0c78e52a3
jaym21 <jaynmistry21@gmail.com>
2022-06-14 12:38:48 +0530

    implemented storing daily goal selected in shared preference

--- app/src/main/java/dev/jaym21/trackin/ui/onboard/DailyGoalFragment.kt
+++ app/src/main/java/dev/jaym21/trackin/ui/onboard/DailyGoalFragment.kt
@@ -1,5 +1,7 @@
 package dev.jaym21.trackin.ui.onboard
 
+import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -7,17 +9,22 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import androidx.navigation.fragment.findNavController
+import dagger.hilt.android.AndroidEntryPoint
 import dev.jaym21.trackin.R
 import dev.jaym21.trackin.databinding.FragmentDailyGoalBinding
 import dev.jaym21.trackin.ui.MainActivity
 import dev.jaym21.trackin.util.Constants
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class DailyGoalFragment : Fragment() {
 
     private var _binding: FragmentDailyGoalBinding? = null
     private val binding: FragmentDailyGoalBinding
         get() = _binding!!
     private val goalDistance = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30)
+    @Inject
+    lateinit var sharedPreferences: SharedPreferences
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -40,6 +47,13 @@ class DailyGoalFragment : Fragment() {
         }
 
         binding.ivSubmit.setOnClickListener {
+            sharedPreferences.edit()
+                .putInt(Constants.DISTANCE_GOAL, binding.spinnerDistance.selectedItem as Int)
+                .putBoolean(Constants.IS_FIRST_RUN, false)
+                .apply()
+
+            startActivity(Intent(requireActivity(), MainActivity::class.java))
+            requireActivity().finish()
         }
     }
 
