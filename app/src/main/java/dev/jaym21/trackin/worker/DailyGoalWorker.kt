f0fdc2003d463407f1c20baaf179e0ce0e8070db
jaym21 <jaynmistry21@gmail.com>
2022-06-16 11:40:50 +0530

    implemented daily goal worker

--- app/src/main/java/dev/jaym21/trackin/worker/DailyGoalWorker.kt
+++ app/src/main/java/dev/jaym21/trackin/worker/DailyGoalWorker.kt
@@ -3,12 +3,15 @@ package dev.jaym21.trackin.worker
 import android.content.Context
 import android.content.SharedPreferences
 import androidx.hilt.work.HiltWorker
+import androidx.work.OneTimeWorkRequest
+import androidx.work.WorkManager
 import androidx.work.Worker
 import androidx.work.WorkerParameters
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedInject
 import dev.jaym21.trackin.util.Constants
 import dev.jaym21.trackin.util.Utilities
+import java.util.concurrent.TimeUnit
 
 @HiltWorker
 class DailyGoalWorker @AssistedInject constructor(private val sharedPreferences: SharedPreferences, @Assisted context: Context, @Assisted params: WorkerParameters): Worker(context, params) {
@@ -26,5 +29,12 @@ class DailyGoalWorker @AssistedInject constructor(private val sharedPreferences:
 
     private fun enqueueNextWorker() {
         val delay = Utilities.getNextDayTimestamp() - System.currentTimeMillis()
+        if (delay > 0) {
+            val dailyWork = OneTimeWorkRequest.Builder(DailyGoalWorker::class.java)
+                .setInitialDelay(delay, TimeUnit.MILLISECONDS)
+                .build()
+
+            WorkManager.getInstance(applicationContext).enqueue(dailyWork)
+        }
     }
 }
